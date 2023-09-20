0c7c746420fea240a602db54c69d8a2413b793cd
Ramiro Correa <98496459+ramicorrea21@users.noreply.github.com>
2023-09-20 00:15:44 +0000

    trafic light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+
+import React  from "react";
 import "../../styles/index.css";
+import { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [selected, setSelected] = useState("")
+
 	return (
 	<>
+	<div className="trafficTop">
+		
+	</div>
+	<div className="container traffic rounded">
+		<div className={selected === "red"? "selected light red" :  "light red"} onClick={() => setSelected("red") }>
+
+		</div>
+		<div className={selected === "yellow"? "selected light yellow" :  "light yellow" } onClick={() => setSelected("yellow") }>
+			
+		</div>
+		<div className={selected === "green"? "selected light green" :  "light green"} onClick={() => setSelected("green") } >
+			
+		</div>
+	</div>
 	</>
 	);
 };
